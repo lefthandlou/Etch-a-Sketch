8d69276572cbcac1b829028f65cb509f6923bce4
lefthandlou <lulu.bestvater@gmail.com>
2022-05-20 08:39:20 -0400

    Separate reset button from make grid button, make grid button clickable only once per site load.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let resetButton = document.querySelector('.reset');
 
+const sketchContainer = document.querySelector('.sketchContainer'); 
 
 //Prompt user to input number
 function pickNumber() {
@@ -8,40 +8,58 @@ function pickNumber() {
 }
 
 //Use for loop to create as many new divs as user specifies in prompt
-//TODO - take user input and make grid, not just total number
+//TODO - make grid a block, with no orphans
+
 function newDiv() {
     let userInput=pickNumber();
-    for (let i=0; i<userInput; i++) {
-        const newContainer = document.createElement('div');
-        const sketchContainer = document.querySelector('.sketchContainer'); 
+    let gridSize = (userInput)*(userInput);
+    for (let i=0; i<gridSize; i++) {
+        let newContainer = document.createElement('div');
         newContainer.classList.add('divs');
         sketchContainer.append(newContainer);
         newContainer.style.border = '1px solid #80ffdb';
         newContainer.style.width = '100px';
-        newContainer.style.height = '100px';
-    };
+        newContainer.style.height = '100px'; 
+    }; 
+    const sketchCells = document.querySelectorAll('.divs'); 
+    //console.log(sketchCells);
+    return sketchCells;
 }
+
 
 //event listener to hook user input and new div creation to button click
 function windowPrompt() {
-    resetButton.addEventListener('click', function handleclick(event){
+    let gridButton = document.querySelector('.setGrid');
+    gridButton.addEventListener('click', function handleclick(event){
        newDiv();
+       gridButton.disabled=true;
     }); 
 } 
 
+//event listener to refresh page on click
+function reloadPage() {
+    let resetButton=document.querySelector('.reset');
+    resetButton.addEventListener('click', () => location.reload());
+	windowPrompt();	 
+}
+
+//TODO set color schemes for drawing mode
+
+//TODO make nodelist from newContainer for use in drawing function
 
 
-/*Function hoverListener () {
-    When any of the divs created above are hovered (mouse in mouse out?) over by user
-    change class name for new CSS styling for the color in that div to either a set color or a 
+//Working mouseout for main sketchpad container below
+/*sketchContainer.addEventListener('mouseout' , function changeColor(event) {
+    sketchContainer.classList.add('color');
+})*/
+
+
+/*TODO Function hoverListener () {
+    foreach addEventListener('mouseout') in all of new divs created; mouseout by user
+    changes class name for new CSS styling for the color in that div to either a set color or a 
     rainbow gradient. If re-hovered, do not change again.
-}
+}*/
 
-Function resetSketch () {
-    Click 'reset' button to clear away sketch and grid 
-    and re-prompt user to set grid size/color setting.
-}
 
-*/
 
-console.log(windowPrompt());
+console.log(reloadPage());
