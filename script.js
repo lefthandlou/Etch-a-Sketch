807f12abc493579fd3b8e3f5750ca0a7728f0a7e
lefthandlou <lulu.bestvater@gmail.com>
2022-05-23 13:31:38 -0400

    Separation of concerns; limited user input to 100

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 
 const sketchContainer = document.querySelector('.sketchContainer'); 
 
-//Prompt user to input number for size of grid
-//TODO format user input for squared grid/error message
-//TODO limit number that can be input with if/else
+//Prompt user to input number for size of grid 
+// and limit to 100 or less to prevent crashing
+
 function pickNumber() {
-    let userInput=window.prompt('input a number');            
-    return userInput;  
+    let userInput=window.prompt('input a number');  
+    if (userInput >= 100) {
+        window.alert('Please input a number less than 100');
+        let userInput=window.prompt('input a number');  
+        return userInput;
+    } else if (userInput <= 100) {
+        return userInput;  
+        
+    };          
+}
+
+//For loop creates new divs per user specification, squared
+function newDiv() {
+    let userInput = pickNumber();
+    let gridSize = (userInput)*(userInput);
+    for (let i=0; i<gridSize; i++) {
+        let newContainer = document.createElement('div');
+        newContainer.classList.add('divs');
+        sketchContainer.append(newContainer); 
+        let cellSize = ((1000/userInput)-1);
+        let cellSizeString=(cellSize.toString() + 'px');
+        newContainer.style.height = (cellSizeString);
+        newContainer.style.width = (cellSizeString);
+    }; 
+}
+
+function colorCells() {
+    let makeDiv = newDiv();
+    const sketchCells = document.querySelectorAll('.divs'); 
+    console.log(sketchCells);
+    sketchCells.forEach(cell => {
+        cell.addEventListener('mouseout', function handleHover(event) {
+            let cellColor=backgroundColor();
+            cell.style.backgroundColor = (cellColor);
+        });
+    });
 }
 
 //TODO tie background color mode to buttons
@@ -22,37 +56,6 @@ function backgroundColor(){
     return stringColor;
 }
 
-
-//For loop creates new divs per user specification, squared
-//TODO separation of concerns - split into two functions
-function newDiv() {
-    let userInput = pickNumber();
-    let gridSize = (userInput)*(userInput);
-    for (let i=0; i<gridSize; i++) {
-        let newContainer = document.createElement('div');
-        newContainer.classList.add('divs');
-        sketchContainer.append(newContainer); 
-        //Use JS to set main container height/width and divide that?
-        let cellHeight = ((1000/userInput)-1);
-        let cellWidth = ((1000/userInput)-1);
-        let cellHeightString=(cellHeight.toString() + 'px');
-        let cellWidthString= (cellWidth.toString() + 'px');
-        console.log(cellHeightString);
-        console.log(cellWidthString);
-        newContainer.style.height= (cellHeightString);
-        newContainer.style.width = (cellWidthString);
-    }; 
-    
-    const sketchCells = document.querySelectorAll('.divs'); 
-    console.log(sketchCells);
-    sketchCells.forEach(cell => {
-        cell.addEventListener('mouseout', function handleHover(event) {
-            let cellColor=backgroundColor();
-            cell.style.backgroundColor = (cellColor);
-        });
-    });
-}
-
 //TODO rearrage so grid size prompt window pops up when reset button is clicked
 //and get rid of 'click me' grid sizer button
 
@@ -60,7 +63,7 @@ function newDiv() {
 function windowPrompt() {
     let gridButton = document.querySelector('.setGrid');
     gridButton.addEventListener('click', function handleclick(event){
-       newDiv();
+       colorCells();
        gridButton.disabled=true;
     }); 
 } 
@@ -71,6 +74,5 @@ function reloadPage() {
     resetButton.addEventListener('click', () => location.reload());
 	windowPrompt();	 
 }
-
 
 console.log(reloadPage());
