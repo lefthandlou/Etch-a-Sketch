5f593e122d28cbe18f703b7846e1986c0355a461
lefthandlou <lulu.bestvater@gmail.com>
2022-05-26 09:39:55 -0400

    Simplify for now to move on to next segment. Come back to polish later.

--- script.js
+++ script.js
@@ -1,65 +1,60 @@
-
-const sketchContainer = document.querySelector('.sketchContainer'); 
-
-//Prompt user to input number for size of grid 
-// and limit to 100 or less to prevent crashing
-
+//Limit user input to 100
 function pickNumber() {
     let userInput=window.prompt('input a number');  
-    if (userInput >= 100) {
+    if (userInput > 100) {
         window.alert('Please input a number less than 100');
         let userInput=window.prompt('input a number');  
         return userInput;
     } else if (userInput <= 100) {
         return userInput;  
-        
     };          
 }
 
-//For loop creates new divs per user specification, squared
+//Squares off and evenly sets height/width for new divs
+//TODO separate concerns
 function newDiv() {
     let userInput = pickNumber();
+    const sketchContainer = document.querySelector('.sketchContainer'); 
     let gridSize = (userInput)*(userInput);
     for (let i=0; i<gridSize; i++) {
         let newContainer = document.createElement('div');
         newContainer.classList.add('divs');
         sketchContainer.append(newContainer); 
-        let cellSize = ((1000/userInput)-1);
+        let cellSize = ((700/userInput)-1);
         let cellSizeString=(cellSize.toString() + 'px');
         newContainer.style.height = (cellSizeString);
         newContainer.style.width = (cellSizeString);
     }; 
 }
 
-function colorCells() {
-    let makeDiv = newDiv();
-    const sketchCells = document.querySelectorAll('.divs'); 
-    console.log(sketchCells);
-    sketchCells.forEach(cell => {
-        cell.addEventListener('mouseout', function handleHover(event) {
-            let cellColor=backgroundColor();
-            cell.style.backgroundColor = (cellColor);
-        });
-    });
-}
 
-//TODO tie background color mode to buttons
-//TODO grayscale color mode
-//TODO all-black color mode
-//Randomly select color from selection to put as div background
-function backgroundColor(){
+//Randomly select color from array to put as div background
+function rainbowBackground(){
     let colorSelections = ['#7401b8', '#692fc3', '#5e60ce', '#5390d9', '#4fa8de', 
-    '#55cfe1', '#72efdd'];
+        '#55cfe1', '#72efdd'];
     let pickColor = colorSelections[Math.floor(Math.random()
         *colorSelections.length)];
     let stringColor = pickColor.toString();
     return stringColor;
 }
 
-//TODO rearrage so grid size prompt window pops up when reset button is clicked
-//and get rid of 'click me' grid sizer button
+//Calls newDiv function to access the nodelist to iterate over cells and add color
+//TODO limit eventlistener to one event or remove after event so coloring
+// over doesn't happen
+function colorCells() {
+    let makeDiv = newDiv();
+    const sketchCells = document.querySelectorAll('.divs'); 
+    sketchCells.forEach(cell => {
+        cell.addEventListener('mouseover', function handleHover(event) {
+            let cellColor=rainbowBackground();
+            cell.style.backgroundColor = (cellColor);
+        });
+    });
+}
 
-//event listener to hook user input and new div creation to button click
+//TODO make initial grid colorable
+//event listener to hook user input, new div creation,
+//and cell color to button click
 function windowPrompt() {
     let gridButton = document.querySelector('.setGrid');
     gridButton.addEventListener('click', function handleclick(event){
